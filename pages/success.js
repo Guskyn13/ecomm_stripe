3d687f6e71be5b5ea1e9a397e00c00087db1b9f4
nathan gusky <nathangusky13@gmail.com>
2022-05-20 04:33:32 -0600

    first commit

---
+++ pages/success.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+import { BsBagCheckFill } from 'react-icons/bs'
+import { router } from 'next/router'
+import { useStateContext } from '../context/StateContext'
+
+const Success = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Success
